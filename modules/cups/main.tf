e3949acc8931a99a23b60d1c388c8e7a3e5b2adf
cezarmathe <me@cezarmathe.com>
2020-11-17 10:59:24 +0200

    Add cups module

---
+++ modules/cups/main.tf
@@ -0,0 +1,67 @@
+# home-server/cups
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+    docker = {
+      source = "terraform-providers/docker"
+      version = "~> 2.7.2"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+# Docker provider.
+provider "docker" {
+  host = var.docker_host
+}
+
+# Cloudflare provider.
+provider "cloudflare" {
+  api_token = var.cf_api_token
+}
+
+# DNS record for the CUPS server.
+resource "cloudflare_record" "cups_service" {
+  zone_id  = var.cf_zone_id
+  name     = var.cf_record_name
+  value    = var.cf_record_value
+  type     = var.cf_record_type
+  proxied  = var.cf_record_proxied
+  ttl      = 1
+}
+
+data "docker_registry_image" "cups" {
+  name = "ydkn/cups:${var.image_version}"
+}
+
+resource "docker_image" "cups" {
+  name          = data.docker_registry_image.cups.name
+  pull_triggers = [data.docker_registry_image.cups.sha256_digest]
+}
+
+# CUPS config Docker volume.
+resource "docker_volume" "cups_config" {
+  name        = "cups_config"
+  driver      = "local-persist"
+  driver_opts = {
+    mountpoint = var.config_mountpoint
+  }
+}
+
+resource "docker_container" "cups" {
+  name  = "cups"
+  image = docker_image.cups.latest
+
+  volumes {
+    volume_name    = docker_volume.cups_config.name
+    container_path = "/etc/cups"
+  }
+
+  must_run = true
+  restart  = "unless-stopped"
+  start    = true
+}
